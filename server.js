73eceee7d92231057f18178549559269701e8aee
Akshay Choudhary <ak92choudhary@gmail.com>
2018-04-27 23:06:20 -0400

    created Models

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const user = require('./routes/api/user');
+const user = require('./routes/api/users');
 const profile = require('./routes/api/profile');
-const post = require('./routes/api/post');
+const post = require('./routes/api/posts');
 
 
 const app = express();
@@ -23,9 +23,9 @@ app.get('/', (req, res) => {
 });
 
 //use routes
-app.use('/api/user' , user);
+app.use('/api/users' , user);
 app.use('/api/profile' , profile);
-app.use('/api/post' , post);
+app.use('/api/posts' , post);
 
 const port = process.env.PORT || 5000;
 
